c2be613ad418504963e1d63fa7e27a3993823c5a
MaxJGruber <maxjgruber@gmail>
2026-02-07 11:06:56 +0100

    Add provisioning scripts

---
+++ apply_all.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+kubectl apply -f ./k8s/namespace.yaml
+kubectl apply -f ./k8s/secrets.yaml
+kubectl apply -f ./k8s/mongo-pvc.yaml
+kubectl apply -f ./k8s/mongo.yaml
+kubectl apply -f ./k8s/redis.yaml
+kubectl apply -f ./k8s/backend.yaml
+kubectl apply -f ./k8s/frontend.yaml
